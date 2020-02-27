b61ddb8bf60a55f51b73393edf83efe5231b96a8
etgreer91 <etgreer91@gmail.com>
2020-02-27 12:06:48 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(games) {
+  let game_won = games.find(game => {return game.result === "W"})
+  if(game_won === undefined) {
+    return undefined
+  } else {
+    return game_won.year
+  }
+}
